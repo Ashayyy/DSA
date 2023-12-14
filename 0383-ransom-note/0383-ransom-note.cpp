6c179bc15e60342f273ac37b6757a02736ac0199
Ashayyy <148455755+Ashayyy@users.noreply.github.com>
2023-12-15 01:21:05 +0530

    Time: 19 ms (42.06%), Space: 9.1 MB (63.36%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        
+        unordered_map<char,int> m;
+        
+        for(int i=0;i<magazine.size();i++){
+            m[magazine[i]]++;
+        }
+        
+        for(int i=0;i<ransomNote.size();i++){
+            m[ransomNote[i]]--;
+            if(m[ransomNote[i]]<0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
