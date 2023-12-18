d224ab02e3f2dbf005869c7baedb278caa931609
Ashayyy <148455755+Ashayyy@users.noreply.github.com>
2023-12-18 15:21:10 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (39.10%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+       
+        s = s +" ";
+        
+        int i=0,j=0;
+        unordered_map<string,char> m1;
+        unordered_map<char,string> m2;
+        
+        while(i<pattern.size() && j<s.size()){
+            
+            string temp="";
+    
+            while(s[j] != ' '){
+                temp = temp + s[j];
+                j++;
+            }
+            
+            j++;
+            
+            if( m1.count(temp) && m1[temp] != pattern[i] )
+                return false;
+            
+            if( m2.count(pattern[i]) && m2[pattern[i]] != temp )
+                return false;
+            
+            m1[temp] = pattern[i];
+            m2[pattern[i]]=temp;
+            
+            i++;
+        }
+        
+        if(j<s.size() || i<pattern.size())
+            return false;
+        
+        return true;
+        
+    }
+};
