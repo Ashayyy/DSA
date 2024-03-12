64559b0abed38faa6f25c8c9fbbe5442e9f585eb
Ashayyy <148455755+Ashayyy@users.noreply.github.com>
2024-03-13 00:42:05 +0530

    Time: 23 ms (82.30%), Space: 23.7 MB (65.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,17 +2,23 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        unordered_map<string,vector<string>> m;
+        //  taking an container to store string
         vector<vector<string>> ans;
+        
+        // storing occurence of each word of different form
+        unordered_map<string,vector<string>> m;
+        
+        
         for(int i=0;i<strs.size();i++){
             string word = strs[i];
             sort(word.begin(),word.end());
             m[word].push_back(strs[i]);
         }
         
-        for(auto i = m.begin() ;i != m.end() ;i ++){
-            ans.push_back(i->second);
+        for(auto it = m.begin(); it!=m.end();it++){
+            ans.push_back(it->second);
         }
+        
         return ans;
     }
 };
